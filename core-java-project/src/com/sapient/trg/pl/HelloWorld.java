50b39dd2c9ca9ee4840b385e379dcd22b362587e
bhushanchauhan477 <bhushanchauhan477@gmail.com>
2021-04-20 21:40:27 +0530

    New commit

---
+++ core-java-project/src/com/sapient/trg/pl/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.sapient.trg.pl;
+import java.util.Calendar;
+import java.util.Date;
+/**
+ * 
+ * @author bhusachd
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println(" Hello World!");
+		System.out.println(" Today date: "+ new Date());
+		System.out.println(Calendar.getInstance());
+		// TODO Auto-generated method stub
+
+	}
+
+}
